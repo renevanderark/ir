dc84d13be5640f8b312c450f4e84c7da5b842948
Rene van der Ark <r.van.der.ark@gmail.com>
2017-05-26 11:23:23 +0200

    adds status codes response.

--- src/main/java/nl/kb/dare/endpoints/RecordStatusEndpoint.java
+++ src/main/java/nl/kb/dare/endpoints/RecordStatusEndpoint.java
@@ -2,11 +2,18 @@ package nl.kb.dare.endpoints;
 
 import nl.kb.dare.model.preproces.RecordReporter;
 import nl.kb.dare.model.reporting.ErrorReporter;
+import nl.kb.dare.model.statuscodes.ErrorStatus;
+import nl.kb.dare.model.statuscodes.ProcessStatus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 @Path("/record-status")
 public class RecordStatusEndpoint {
@@ -31,5 +38,22 @@ public class RecordStatusEndpoint {
     public Response getErrorStatus() {
 
         return Response.ok(errorReporter.getStatusUpdate().getData()).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/status-codes")
+    public Response getStatusCodes() {
+        final Map<String, Map<Integer, String>> statusCodes = new HashMap<>();
+
+        statusCodes.put("errorStatuses",
+                Stream.of(ErrorStatus.values()).collect(toMap(ErrorStatus::getCode, ErrorStatus::getStatus)));
+
+        statusCodes.put("processStatuses",
+                Stream.of(ProcessStatus.values()).collect(toMap(ProcessStatus::getCode, ProcessStatus::getStatus)));
+
+        return Response
+                .ok(statusCodes)
+                .build();
     }
 }
