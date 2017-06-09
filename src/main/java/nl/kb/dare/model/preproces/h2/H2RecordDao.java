7d14afe8a7e68f42c1aa60a4d71d42d9f0e2cb8f
Rene van der Ark <r.van.der.ark@gmail.com>
2017-06-09 09:01:22 +0200

    initial h2 setup.

---
+++ src/main/java/nl/kb/dare/model/preproces/h2/H2RecordDao.java
@@ -0,0 +1,18 @@
+package nl.kb.dare.model.preproces.h2;
+
+import nl.kb.dare.model.preproces.Record;
+import nl.kb.dare.model.preproces.RecordDao;
+import nl.kb.dare.model.preproces.RecordMapper;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
+import java.util.List;
+
+@RegisterMapper(RecordMapper.class)
+public abstract class H2RecordDao implements RecordDao {
+
+    @SqlBatch("INSERT INTO DARE_PREPROCES (STATE, KBOBJID, TS_CREATE, REPOSITORY_ID, OAI_ID, OAI_DATESTAMP) " +
+            "VALUES (:state, :kbObjId, CURRENT_TIMESTAMP, :repositoryId, :oaiIdentifier, :oaiDateStamp)")
+    public abstract void insertBatch(@BindBean List<Record> recordList);
+}
---
+++ src/main/resources/database-schema/h2/repositories.sql
@@ -0,0 +1,12 @@
+CREATE TABLE repositories (
+  id number(10) AUTO_INCREMENT,
+  name VARCHAR2(255) DEFAULT NULL,
+  url varchar2(255) DEFAULT NULL,
+  metadataPrefix varchar2(255) DEFAULT NULL,
+  oai_set varchar2(50) DEFAULT NULL,
+  datestamp varchar2(50) DEFAULT NULL,
+  schedule NUMBER(10) NOT NULL,
+  enabled NUMBER(3) DEFAULT 0 NOT NULL,
+  lastHarvest TIMESTAMP(6) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
