b87fe3339969a7f9857b6862b5a064710c5c27ea
Rene van der Ark <r.van.der.ark@gmail.com>
2017-06-12 14:23:00 +0200

    new setnames.

--- src/main/java/nl/kb/dare/tasks/LoadRepositoriesTask.java
+++ src/main/java/nl/kb/dare/tasks/LoadRepositoriesTask.java
@@ -22,17 +22,17 @@ public class LoadRepositoriesTask extends Task {
         final String dateStamp = params.containsKey("datestamp")
                 ? params.get("datestamp").iterator().next() : null;
         Lists.newArrayList(
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Utrecht", "nl_didl_norm", "uu", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Nijmegen", "nl_didl_norm", "ru", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Groningen", "nl_didl_norm", "rug", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Delft", "nl_didl_norm", "tud", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Leiden", "nl_didl_norm", "ul", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Maastricht", "nl_didl_norm", "um", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Twente", "nl_didl_norm", "ut", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "UvA", "nl_didl_norm", "uva", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Tilburg", "nl_didl_norm", "uvt", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "VU", "nl_didl_norm", "vu", dateStamp, false, HarvestSchedule.DAILY),
-            new Repository("http://oai.gharvester.dans.knaw.nl/", "Wageningen", "nl_didl_norm", "wur", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Utrecht", "nl_didl_norm", "uu:dare", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Nijmegen", "nl_didl_norm", "ru:col_2066_13799", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Groningen", "nl_didl_norm", "rug:publications:withFiles", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Delft", "nl_didl_norm", "tud:A-set", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Leiden", "nl_didl_norm", "ul:hdl_1887_4539", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Maastricht", "nl_didl_norm", "um:publications:withFiles", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Twente", "nl_didl_norm", "ut:66756C6C746578743D7075626C6963", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "UvA", "nl_didl_norm", "uva:publications:withFiles", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Tilburg", "nl_didl_norm", "uvt:publications:withFiles", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "VU", "nl_didl_norm", "vu:publications:withFiles", dateStamp, false, HarvestSchedule.DAILY),
+            new Repository("http://oai.gharvester.dans.knaw.nl/", "Wageningen", "nl_didl_norm", "wur:publickb", dateStamp, false, HarvestSchedule.DAILY),
             new Repository("http://oai.gharvester.dans.knaw.nl/", "Rotterdam", "nl_didl_norm", "eur", dateStamp, false, HarvestSchedule.DAILY)
         ).forEach(this.repositoryDao::insert);
     }
