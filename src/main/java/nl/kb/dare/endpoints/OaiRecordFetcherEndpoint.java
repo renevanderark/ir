875fd2aa906fa039eedefd8444639f37b86881c3
Rene van der Ark <r.van.der.ark@gmail.com>
2017-06-14 10:35:44 +0200

    adds auth filter to record fetcher endpoint.

--- src/main/java/nl/kb/dare/endpoints/OaiRecordFetcherEndpoint.java
+++ src/main/java/nl/kb/dare/endpoints/OaiRecordFetcherEndpoint.java
@@ -3,6 +3,7 @@ package nl.kb.dare.endpoints;
 import nl.kb.dare.endpoints.kbaut.KbAuthFilter;
 import nl.kb.dare.jobs.ScheduledOaiRecordFetcher;
 
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,9 +11,11 @@ import javax.ws.rs.core.Response;
 
 @Path("/workers")
 public class OaiRecordFetcherEndpoint {
+    private final KbAuthFilter filter;
     private final ScheduledOaiRecordFetcher oaiRecordFetcher;
 
     public OaiRecordFetcherEndpoint(KbAuthFilter filter, ScheduledOaiRecordFetcher oaiRecordFetcher) {
+        this.filter = filter;
 
         this.oaiRecordFetcher = oaiRecordFetcher;
     }
@@ -20,16 +23,22 @@ public class OaiRecordFetcherEndpoint {
     @PUT
     @Path("/start")
     @Produces("application/json")
-    public Response start() {
-        oaiRecordFetcher.enable();
-        return Response.ok("{}").build();
+    public Response start(@HeaderParam("Authorization") String auth) {
+
+        return filter.getFilterResponse(auth).orElseGet(() -> {
+            oaiRecordFetcher.enable();
+            return Response.ok("{}").build();
+        });
     }
 
     @PUT
     @Path("/disable")
     @Produces("application/json")
-    public Response disable() {
-        oaiRecordFetcher.disable();
-        return Response.ok("{}").build();
+    public Response disable(@HeaderParam("Authorization") String auth) {
+
+        return filter.getFilterResponse(auth).orElseGet(() -> {
+            oaiRecordFetcher.disable();
+            return Response.ok("{}").build();
+        });
     }
 }
--- src/main/scripts/node/src/actions.js
+++ src/main/scripts/node/src/actions.js
@@ -6,7 +6,6 @@ import {
     setHarvestSchedule,
     validateNewRepository,
     saveRepository,
-    fetchRepositories,
     deleteRepository
 } from "./actions/repositories";
 
@@ -14,13 +13,20 @@ import {
     startHarvest,
     interruptHarvest
 } from "./actions/harvesters";
+
 import {fetchInitialData} from "./bootstrap";
+import {handleResponse} from "./actions/response-handler";
+
 
 const startOaiRecordFetcher = () => (dispatch) =>
-    xhr({url: "/workers/start", "method": "PUT"}, (err, resp, body) => {});
+    xhr({url: "/workers/start", "method": "PUT", headers: { 'Authorization': localStorage.getItem("authToken") }},
+        (err, resp, body) => handleResponse(resp));
+
 
 const disableOaiRecordFetcher = () => (dispatch) =>
-    xhr({url: "/workers/disable", "method": "PUT"}, (err, resp, body) => {});
+    xhr({url: "/workers/disable", "method": "PUT", headers: { 'Authorization': localStorage.getItem("authToken") }},
+        (err, resp, body) => handleResponse(resp));
+
 
 export default function actionsMaker(navigateTo, dispatch) {
     return {
