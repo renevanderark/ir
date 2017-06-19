d0762ea56e93ab38852d6a9c53c8b1927f481224
Rene van der Ark <r.van.der.ark@gmail.com>
2017-06-19 16:21:54 +0200

    adds record detail

--- src/main/scripts/node/src/components/record-status/record-status.js
+++ src/main/scripts/node/src/components/record-status/record-status.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Panel from "../layout/panel";
 
 class RecordStatus extends React.Component {
 
@@ -20,10 +21,87 @@ class RecordStatus extends React.Component {
 
 
     render() {
+        const { record, errorReport, processStatuses, repositories, errorStatuses } = this.props;
+        if (!record) { return null; }
+
+        const errorReportPanel = errorReport
+            ? (
+                <Panel title="Foutrapport">
+                    <div className="row">
+                        <label className="col-md-6">Statuscode</label>
+                        <div className="col-md-26">
+                            {errorReport.statusCode} - {errorStatuses[errorReport.statusCode] || ""}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Melding</label>
+                        <div className="col-md-26">
+                            {errorReport.message}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">URL</label>
+                        <div className="col-md-26">
+                            <a href={errorReport.url} target="_blank">
+                                {errorReport.url}
+                            </a>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Stacktrace</label>
+                        <pre className="col-md-26">
+                            {errorReport.stackTrace}
+                        </pre>
+                    </div>
+                </Panel>
+            )
+            : null;
+
         return (
-            <pre>
-                {JSON.stringify(this.props, null, 2)}
-            </pre>
+            <div>
+                <Panel title="Overzicht IP">
+                    <div className="row">
+                        <label className="col-md-6">OAI/PMH identifier</label>
+                        <div className="col-md-26">
+                            <a target="_blank"
+                               href={`http://oai.gharvester.dans.knaw.nl/?verb=GetRecord&metadataPrefix=nl_didl_norm&identifier=${encodeURIComponent(record.oaiIdentifier)}`}>
+                                {record.oaiIdentifier}
+                            </a>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">KB object identifier</label>
+                        <div className="col-md-26">
+                            {record.kbObjId}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Status</label>
+                        <div className="col-md-26">
+                            {processStatuses[record.state]}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Bron</label>
+                        <div className="col-md-26">
+                            {repositories.find(repo => repo.id === record.repositoryId).name}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Aangemaakt</label>
+                        <div className="col-md-26">
+                            {record.tsCreate}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="col-md-6">Verwerkt</label>
+                        <div className="col-md-26">
+                            {record.tsProcessed || "-"}
+                        </div>
+                    </div>
+                </Panel>
+                {errorReportPanel}
+            </div>
         );
     }
 }
--- src/main/scripts/node/src/connectors/record-status-connector.js
+++ src/main/scripts/node/src/connectors/record-status-connector.js
@@ -1,6 +1,9 @@
 export default (state, routed) => ({
     kbObjId: routed.params.kbObjId,
+    repositories: state.repositories.list,
     record: (state.records.record || {}).record,
     errorReport: (state.records.record || {}).errorReport,
-    processStatuses: state.status.statusCodes.processStatuses
+    processStatuses: state.status.statusCodes.processStatuses,
+    errorStatuses: state.status.statusCodes.errorStatuses
+
 })
