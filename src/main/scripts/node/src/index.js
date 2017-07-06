54757f0376e3d4744d75d3c09e708e861ac03c13
Rene van der Ark <r.van.der.ark@gmail.com>
2017-07-06 14:00:33 +0200

    adds force reload at 30 second intervals.

--- src/main/scripts/node/src/index.js
+++ src/main/scripts/node/src/index.js
@@ -5,6 +5,12 @@ import ReactDOM from "react-dom";
 import router from "./router";
 
 import { authenticateAndInitialize } from "./bootstrap";
+import {fetchInitialData} from "./actions/fetch-initial-data";
 
 // Authenticate, initialize render data and then render the app to the DOM, registering the router
 authenticateAndInitialize(() => ReactDOM.render(router, document.getElementById("app")));
+
+setInterval(
+    () => fetchInitialData(() => console.log(`Force reload at ${new Date().getTime()}`)),
+    30 * 1000
+);
